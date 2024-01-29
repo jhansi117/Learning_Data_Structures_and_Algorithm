f5e78bcdcdcbd96444be5bbf39afe94d7a1f5b2d
Jhansi <jhansiranimathivanan@gmail.com>
2024-01-29 15:13:26 +0530

    Today I learnt all the operators in Java.

---
+++ 01_operators/Assignment.java
@@ -0,0 +1,14 @@
+public class Assignment {
+    public static void main(String args[]){
+        int A = 10;
+        int B = 5;
+        // A += B;
+        // System.out.println(A);
+        // A -= B;
+        // System.out.println(A);
+        // A *= B;
+        // System.out.println(A);
+        A /= B;
+        System.out.println(A);
+    }
+}
---
+++ 01_operators/Logical.java
@@ -0,0 +1,7 @@
+public class Logical {
+    public static void main(String args[]){
+        System.out.println(5>2 && 5>7);
+        System.out.println((5>2) || (3>8));
+        System.out.println(!(5>2));
+    }
+}
